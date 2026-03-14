f70929d2b6f4a752346ac179448dc663e1bcdb70
HamitMizrak <hamitmizrak@gmail.com>
2026-03-14 10:06:40 +0300

    primitive type

--- src/main/java/com/hamitmizrak/_03_PrimitiveType.java
+++ src/main/java/com/hamitmizrak/_03_PrimitiveType.java
@@ -1,4 +1,29 @@
 package com.hamitmizrak;
 
 public class _03_PrimitiveType {
+    static void main() {
+        // primitive type(İlker Türler)
+        // stack memory
+
+        // 4 tane sayı
+        byte primitiveByte=127; // -128<=SAYI<=127
+        short primitiveShort=300;
+        int primitiveInt=511565115;
+        long primitiveLong=5115651156L;
+
+        // 2 tane virgüllü(Floating point)
+        float primitiveFloat=14.53F;
+        double primitiveDouble=1453.10711071;
+
+        // boolean (true,false)
+        boolean primitiveBoolean=12>15;  //true;
+
+        // char
+        char primitiveChar='&';
+        System.out.println(primitiveChar);
+
+        char primitiveChar2='\u4235';
+        System.out.println(primitiveChar2);
+    }
+
 }
