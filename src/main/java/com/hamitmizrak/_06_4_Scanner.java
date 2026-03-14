359c79c2ffe7a6aec4187bd4260f27e51f08c681
HamitMizrak <hamitmizrak@gmail.com>
2026-03-14 12:17:39 +0300

    Scanner

---
+++ src/main/java/com/hamitmizrak/_06_4_Scanner.java
@@ -0,0 +1,21 @@
+package com.hamitmizrak;
+
+import java.util.Random;
+import java.util.Scanner;
+
+// Mathematics
+// static düzenlidir
+public class _06_4_Scanner {
+
+    static void main() {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Lütfen adınızı giriniz");
+        String name=scanner.nextLine();
+        System.out.println("Girdiğiniz ad: "+name);
+
+        System.out.println("\nLütfen bir sayı giriniz");
+        int number= scanner.nextInt();
+        System.out.println("Girdiğiniz sayı: "+number);
+
+    }
+}
