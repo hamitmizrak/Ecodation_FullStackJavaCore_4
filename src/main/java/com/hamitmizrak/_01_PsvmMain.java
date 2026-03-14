58e5fbac0889a54258d54c6b80c2f69b40ee0a34
HamitMizrak <hamitmizrak@gmail.com>
2026-03-14 09:43:00 +0300

    psvm

---
+++ src/main/java/com/hamitmizrak/_01_PsvmMain.java
@@ -0,0 +1,10 @@
+package com.hamitmizrak;
+
+// Class Name
+public class _01_PsvmMain {
+    // psvm => TAB tuşu
+    public static void main() {
+        // sout => TAB tuşu
+        System.out.println("Merhabalar Güzel İnsanlar");
+    }
+}
