951433ab155dc68e5164aeee94dd8607530c7b85
HamitMizrak <hamitmizrak@gmail.com>
2026-03-14 09:49:58 +0300

    variable

--- src/main/java/com/hamitmizrak/_02_Variable.java
+++ src/main/java/com/hamitmizrak/_02_Variable.java
@@ -1,4 +1,22 @@
 package com.hamitmizrak;
 
 public class _02_Variable {
+
+    static void main() {
+        int schoolNumber=44;
+        System.out.println(schoolNumber);
+
+        // Dikkat yazılabilir
+
+        // underscore(alt çizgi)
+        int _schoolNumber=23;
+        System.out.println(_schoolNumber);
+
+        // Dolar sembolü
+        int $schoolNumber=23;
+        System.out.println($schoolNumber);
+
+        // sayı ile başlama, özel simge ile başlama
+        // üğşçöİ => başlama
+    }
 }
