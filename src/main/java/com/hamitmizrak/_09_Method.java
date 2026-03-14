a49807a694a86431b8e489c19276fd2ba6a886e5
HamitMizrak <hamitmizrak@gmail.com>
2026-03-14 12:17:47 +0300

    Method

--- src/main/java/com/hamitmizrak/_09_Method.java
+++ src/main/java/com/hamitmizrak/_09_Method.java
@@ -1,8 +1,41 @@
 package com.hamitmizrak;
 
+// Method: VEri tekrarını önleyen demektir.
+// camelCase isimlendirme yapılır
+// isimlendirmede çok çok uzun kelimeler kullanmayız
 public class _09_Method {
 
-    static void main() {
-
+    // 1- voidliParametresiz
+    public static void voidliParametresiz() {
+        System.out.println("1- voidli Parametresiz");
     }
+
+    // 2- voidliParametreli
+    public static void voidliParametreli(int data) {
+        System.out.println("2- voidli Parametreli: " + data);
+    }
+
+    // 3- returnParametresiz
+    public static String returnParametresiz() {
+        return "3- returnlu Parametresiz";
+    }
+
+    // 4- returnParametresiz
+    public static String returnParametreli(String username, int number, String password) {
+        return "4- returnlu Parametreli " + username + " " + password+" "+number;
+    }
+
+
+    // PSVM
+    static void main() {
+        voidliParametresiz();
+        voidliParametreli(44);
+
+        String data3 = returnParametresiz();
+        System.out.println(data3);
+
+        String data4 = returnParametreli("Hamit",44,"Mızrak");
+        System.out.println(data4);
+    }
+
 }
