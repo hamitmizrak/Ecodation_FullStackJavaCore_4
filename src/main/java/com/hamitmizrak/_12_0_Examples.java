f3c40eec9e06a5b4d47f24eaa7aa80ba9318f6db
HamitMizrak <hamitmizrak@gmail.com>
2026-03-15 11:03:58 +0300

    examples

--- src/main/java/com/hamitmizrak/_12_0_Examples.java
+++ src/main/java/com/hamitmizrak/_12_0_Examples.java
@@ -1,6 +1,18 @@
 package com.hamitmizrak;
 
+/*
+* Kullanıcıdan Öğrenci sayısını alalım.
+* Her öğrenci için isim ve not alınır.
+* Ortalama hersaplanır
+* En yüksek notu bulunur.
+* */
 public class _12_0_Examples {
+
+    // PSVM
+    static void main() {
+
+    }
+
 }
 
 // Pazartesi-Salı-Çarşamba-Perşembe
