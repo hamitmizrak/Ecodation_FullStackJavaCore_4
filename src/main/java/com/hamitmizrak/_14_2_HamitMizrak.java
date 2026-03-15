322afd0531258e19aec9e5e672033ae34360ed30
HamitMizrak <hamitmizrak@gmail.com>
2026-03-15 13:37:14 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/_14_2_HamitMizrak.java
@@ -0,0 +1,10 @@
+package com.hamitmizrak;
+
+// try-catch-throw-throws-finally
+public class _14_2_HamitMizrak extends RuntimeException {
+
+    public _14_2_HamitMizrak(String message) {
+        super(message);
+    }
+}
+
