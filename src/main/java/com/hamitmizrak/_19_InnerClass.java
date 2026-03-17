7c3a79a8f1b091fff847ea40a27a67618831d2e3
HamitMizrak <hamitmizrak@gmail.com>
2026-03-17 12:02:57 +0300

    Generics List Set Map Enum Inner Class

---
+++ src/main/java/com/hamitmizrak/_19_InnerClass.java
@@ -0,0 +1,45 @@
+package com.hamitmizrak;
+
+/*
+* InnerClass
+* Javada 1- tane public class vardır(inner class haric)
+* Javada class static olamaz (inner class haric)
+*  */
+
+public class _19_InnerClass {
+
+    private  String message="Dış class";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /// //////////////////////////////
+    // INNER
+    public static class IcClass{
+
+        private String data="iç class verisi";
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+    } // end ic class
+
+
+    static void main() {
+        _19_InnerClass disClass= new _19_InnerClass();
+        System.out.println(disClass.getMessage());
+
+        IcClass icClass= new IcClass();
+        System.out.println(icClass.getData());
+    }
+} // end dış class
+
